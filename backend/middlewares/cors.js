05c9bd8ff8d376eb1178f468185ee911d3cf3e2b
Luis Palacios <luisarmando20092009@gmail.com>
2026-04-21 18:56:54 -0400

    [fix]Update CORS origins for production

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,7 +4,7 @@ const ACCEPTED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:3000',
     'http://localhost:1234',
-    'https://onlygirlsccs-ecommerce-frontend.vercel.app/'
+    'https://onlygirlsccs-ecommerce-frontend.vercel.app'
 ]
 
 export const corsMiddleware =({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>{
